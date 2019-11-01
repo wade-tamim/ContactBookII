4edcd2cc1f7a2a77043a85d511c60f94ad2ef147
Wade <w.tamim@hotmail.com>
2019-11-01 10:41:11 +0100

    Create API controller to get all countries

---
+++ ContactBookUmbracoAPI/API/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using ContactBookUmbracoAPI.API.Models;
+using Umbraco.Core.Services;
+
+namespace ContactBookUmbracoAPI.API.Controllers
+{
+    public class CountriesController : Umbraco.Web.WebApi.UmbracoApiController
+    {
+        // GET: Department
+        public IEnumerable<CountryModel> GetAllCountries()
+        {
+            IContentService contentService = Services.ContentService;
+
+            var CompanyHQChildren = contentService.GetChildren(1066);
+
+            var countriesList = new List<CountryModel>();
+
+            foreach (var country in CompanyHQChildren)
+            {
+                countriesList.Add(new CountryModel()
+                {
+                    Id = country.Id,
+                    Name = country.Name,
+                });
+            }
+
+            return countriesList;
+        }
+
+    }
+
+}
---
+++ ContactBookUmbracoAPI/API/Models/CountryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactBookUmbracoAPI.API.Models
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
